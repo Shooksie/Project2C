e56b34317b29f98fdf87e517adc0c5c8c69e9699
sabby918 <skst4b@mail.umkc.edu>
2016-03-18 17:28:19 -0500

    Update Person.h

--- Project 2-Elevator/Person.h
+++ Project 2-Elevator/Person.h
@@ -1,6 +1,19 @@
+#pragma once
+#include <ctime>
 #include <string>
 
 class person {
+private:
+	int current_floor;
 	int desired_floor;
 	int personID;
+	bool goingUP;
+public:
+	person();
+	int getID();
+	void setID(int number);
+	int getCurrentFloor();
+	int getDesiredFloor();
+	bool getDirection();
+
 };
