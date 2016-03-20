2c8fb87099a0eb777136bd745d564430dd9a6f09
baileyms <bmsv6c@mail.umkc.edu>
2016-03-20 18:57:41 -0500

    Update Elevator.cpp

--- Project 2-Elevator/Elevator.cpp
+++ Project 2-Elevator/Elevator.cpp
@@ -3,14 +3,43 @@
 
 using namespace std;
 
-void elevate::moveUp(){}
-void elevate::moveDown(){}
-int elevate::getLevel(){ return NULL; }
-int elevate::getNumPeople(){ return NULL; }
-bool elevate::isUp(){ return true; }
-bool elevate::hasRequests(){ return true; }
-bool elevate::isDown(){ return true; }
-void elevate::checkPeople(){}
+void elevate::moveUp(){
+	++current_floor;
+	level++;
+	checkPeople();
+}
+void elevate::moveDown(){
+	--current_floor;
+	level--;
+	checkPeople();
+}
+int elevate::getLevel(){ 
+	return level;
+}
+int elevate::getNumPeople(){ 
+	return numPeople;
+}
+bool elevate::isUp(list<int> List){ 
+	if (hasRequests(List) == false)
+		return false;
+	list<int>::iterator itr = List.begin();
+	Up = (*itr > level);
+	return Up;
+}
+bool elevate::hasRequests(list<int> List){
+	return(!List.empty());
+}
+bool elevate::isDown(list<int> List){
+	Down = !isUp(List);
+	return Down;
+}
+void elevate::checkPeople(){
+	list<person>::iterator itr;
+	for (itr = people.begin(); itr != people.end(); ++itr){
+		if (itr->getDesiredFloor() == level)
+			people.erase(itr);
+	}
+}
 void elevate::ConnectItr(list<Floor> List){
 	current_floor = List.begin();
 }
